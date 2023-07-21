7a91f94e197b0998f7b50adf8e6ca9f3d0a587ca
Matheus Inocêncio <inolopesm@gmail.com>
2023-07-20 21:08:34 -0300

    help command

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const main = async () => {
+  const command = process.argv[2];
+
+  if (command === "help") {
+    console.log("=== CONTACTS CRUD ===")
+    console.log("");
+    console.log("Commands:");
+    console.log("- create");
+    console.log("- read");
+    console.log("- update");
+    console.log("- delete");
+    console.log("");
+    console.log("Examples:");
+    console.log("- node src create matheus 550012341234");
+    console.log("- node src read");
+  }
+};
+
+main();
