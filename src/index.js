37a8aec1232ab20ba553141161c1b721830850e5
Matheus Inocêncio <inolopesm@gmail.com>
2023-07-20 21:09:14 -0300

    read command

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+const { createConnection } = require("mysql2/promise");
+
+const config = {
+  user: 'root',
+  password: 'docker',
+  host: 'localhost',
+  port: 3306,
+  database: 'contacts',
+};
+
 const main = async () => {
   const command = process.argv[2];
 
@@ -13,6 +23,18 @@ const main = async () => {
     console.log("Examples:");
     console.log("- node src create matheus 550012341234");
     console.log("- node src read");
+  }
+
+  if (command === "read") {
+    const connection = await createConnection(config);
+    await connection.connect();
+
+    try {
+      const [rows] = await connection.query('SELECT * FROM `Contact`');
+      console.table(rows);
+    } finally {
+      await connection.end();
+    }
   }
 };
 
